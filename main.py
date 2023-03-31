7c03b5479d7bbf04b11d9830de81622f1d875d27
Lucas-Raphael Mueller <git@lrm.dev>
2023-03-31 09:45:32 +0200

    docs

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ async def root():
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
+    # entrypoint for apprunner, because gunicorn directly does not work
+    # since apprunner requires synchronous
+    # workaround:
+    # gunicorn api:app  --bind 0.0.0.0:$PORT --worker-class uvicorn.workers.UvicornWorker
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=8080)
